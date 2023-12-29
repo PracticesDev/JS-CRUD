0be9b79f830a9d02415be865f9866c1776ba816f
Jorge Avella <79177900+jaavella07@users.noreply.github.com>
2023-12-28 20:33:35 -0500

    Creacion de mapper

--- src/users/use-cases/load-users.js
+++ src/users/use-cases/load-users.js
@@ -1,3 +1,5 @@
+import { localhostUserToModel } from "../mappers/localhost-user.mapper";
+import { User } from "../models/user";
 
 
 export const loadUsers = async (page = 1) => {
@@ -5,7 +7,8 @@ export const loadUsers = async (page = 1) => {
     const url = `${import.meta.env.VITE_DB_URL}/users?_page=${page}`;
     const res = await fetch(url);
     const data = await res.json();
+    const users = data.map( localhostUserToModel);
 
-    console.log(data)
+    return users
 
 }
